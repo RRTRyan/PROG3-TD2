30ded4a3fc253b7021d07f90ff9bc72e2ee9ab09
RRTRyan <tiaryanrzk@gmail.com>
2025-12-23 16:46:31 +0300

    fix: use dotenv for jdbc connection

--- Java/src/main/java/org/jdbctd2/DBConnection.java
+++ Java/src/main/java/org/jdbctd2/DBConnection.java
@@ -1,15 +1,19 @@
 package org.jdbctd2;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.*;
 
 public class DBConnection {
+    Dotenv dotenv = Dotenv.load();
+
     public Connection getConnection() {
         try {
             Class.forName("org.postgresql.Driver");
             return DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/mini_dish_db",
-                    "mini_dish_db_manager",
-                    "mini_dish_db"
+                    dotenv.get("POSTGRES_URL"),
+                    dotenv.get("POSTGRES_USER"),
+                    dotenv.get("POSTGRES_PASSWORD")
             );
         } catch (RuntimeException | ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
